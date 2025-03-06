3d533040b88bee7ba7472d5e6e48851d6de4d5a9
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-03-06 09:02:49 +0100

    Insert demo categories

--- EFBlazor/Models/Category/CategoryMapping.cs
+++ EFBlazor/Models/Category/CategoryMapping.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace EFBlazor.Models;
 
-// Konfiguration für die Benutzer-Tabelle
 public class CategoryMapping : IEntityTypeConfiguration<Category>
 {
     public void Configure(EntityTypeBuilder<Category> builder)
@@ -11,6 +10,16 @@ public class CategoryMapping : IEntityTypeConfiguration<Category>
         builder.ToTable("Categories");
 
         builder.HasKey(c => c.CategoryId);
+
+        builder.HasData(
+        [
+            new Category() { CategoryId = 1, Name = "Food" },
+            new Category() { CategoryId = 2, Name = "Toys" },
+            new Category() { CategoryId = 3, Name = "Clothing" },
+            new Category() { CategoryId = 4, Name = "Pets" },
+            new Category() { CategoryId = 5, Name = "Computer" },
+            new Category() { CategoryId = 6, Name = "Books" },
+        ]);
     }
 
 }
